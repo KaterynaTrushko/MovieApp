8eb12b941f86d11125e498b26d06f842726f3ff0
KaterynaTrushko <trushkokateryna@gmail.com>
2022-05-21 16:59:20 +0100

    Ditails

---
+++ src/featuer/movie/showMoviesShowsDetails.js
@@ -0,0 +1,48 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import movieApi from "../../common/apis/movieApi";
+import { APIKey } from "../../common/apis/movieApiKey";
+
+export const fetchAsynkDetails = createAsyncThunk(
+  "showsDitails/fetchAsynkDetails",
+  async (id) => {
+    try {
+      const { data } = await movieApi.get(
+        `?apiKey=${APIKey}&i=${id}&Plot=full`
+      );
+      return data;
+    } catch (error) {
+      console.lof(error.response);
+    }
+  }
+);
+
+const initialState = {
+  data: {},
+  loading: false,
+  isSuccess: false,
+};
+
+export const showsDetailsSlice = createSlice({
+  name: "showsDitails",
+  initialState,
+  reducer: {},
+  extraReducers: {
+    [fetchAsynkDetails.loading]: (state) => {
+      state.loading = true;
+      console.log("loading");
+    },
+    [fetchAsynkDetails.fulfilled]: (state, { payload }) => {
+      state.loading = false;
+      state.data = payload;
+      state.isSuccess = true;
+      console.log(payload);
+    },
+    [fetchAsynkDetails.rejected]: (state, { payload }) => {
+      state.massage = "failed";
+      console.log("failed");
+    },
+  },
+});
+
+export default showsDetailsSlice.reducer;
+export const getDetails = (state) => state.ditails.data;
--- src/featuer/movie/store.js
+++ src/featuer/movie/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import movieReducer from "../movie/movieSlice";
 import showsReducer from "../movie/showsSlice";
+import ShowsDetailsReducer from "./showMoviesShowsDetails";
 
 const store = configureStore({
   reducer: {
     movies: movieReducer,
     shows: showsReducer,
+    ditails: ShowsDetailsReducer,
   },
 });
 
