9f7b316a58132db87355ce045d5fe96dfd7c72fd
KaterynaTrushko <trushkokateryna@gmail.com>
2022-05-25 21:20:44 +0100

    updata

--- src/component/Footer/Footer.js
+++ src/component/Footer/Footer.js
@@ -5,7 +5,10 @@ export default function Footer() {
   return (
     <div className="footer">
       <div>Movie app</div>
-      <div>@2022, Movie, https://www.omdbapi.com</div>
+      <div>
+        @2022, Movie,{" "}
+        <a href="https://www.omdbapi.com">https://www.omdbapi.com</a>
+      </div>
       <i></i>
     </div>
   );
